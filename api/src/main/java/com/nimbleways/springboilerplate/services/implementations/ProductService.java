40169a11a7c2f2723f306ed427b76cd20fd7734f
Ridouan Lamghari <ridouan.lamghari@fasst.io>
2024-11-21 22:17:37 +0100

    Theodo: refactoring test

--- api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
+++ api/src/main/java/com/nimbleways/springboilerplate/services/implementations/ProductService.java
@@ -1,7 +1,8 @@
 package com.nimbleways.springboilerplate.services.implementations;
 
 import java.time.LocalDate;
-
+import com.nimbleways.springboilerplate.entities.Order;
+import com.nimbleways.springboilerplate.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,49 @@ public class ProductService {
 
     @Autowired
     NotificationService ns;
+
+    @Autowired
+    private OrderRepository or;
+
+    private enum ProductType {
+        NORMAL,
+        SEASONAL,
+        EXPIRABLE
+    }
+
+    public Order processOrder(Long orderId) {
+        Order order = or.findById(orderId).get();
+        for (Product p : order.getItems()) {
+            if (p.getType().equals(ProductType.NORMAL.toString())) {
+                if (p.getAvailable() > 0) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    int leadTime = p.getLeadTime();
+                    if (leadTime > 0) {
+                        notifyDelay(leadTime, p);
+                    }
+                }
+            } else if (p.getType().equals(ProductType.SEASONAL.toString())) {
+                // Add new season rules
+                if ((LocalDate.now().isAfter(p.getSeasonStartDate()) && LocalDate.now().isBefore(p.getSeasonEndDate())
+                        && p.getAvailable() > 0)) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    handleSeasonalProduct(p);
+                }
+            } else if (p.getType().equals(ProductType.EXPIRABLE.toString())) {
+                if (p.getAvailable() > 0 && p.getExpiryDate().isAfter(LocalDate.now())) {
+                    p.setAvailable(p.getAvailable() - 1);
+                    pr.save(p);
+                } else {
+                    handleExpiredProduct(p);
+                }
+            }
+        }
+        return order;
+    };
 
     public void notifyDelay(int leadTime, Product p) {
         p.setLeadTime(leadTime);
